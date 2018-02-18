08645d5b6dc5ee59a91f6d4c2a19df2f9888ffd2
neilsatt <neilsatt@gmail.com>
2018-02-18 14:57:56 -0700

    initial commit

---
+++ Buna/101.js
@@ -0,0 +1,16 @@
+/*
+  variable = represent a storage unit/place in the computer memory
+
+  expression = a set of literals, variables, operators, and/or expressions that evaluates to a single value
+
+  operator = assign values, compare values, perform arithmetic operations, etc. 
+
+
+
+
+
+
+
+
+
+*/
